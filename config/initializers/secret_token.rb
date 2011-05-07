0b4829ab1f4d442ddf4f82b144d34913d0e41507
vhariths <vedu.hariths@nsquaredglobal.com>
2011-05-06 22:04:22 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PracticeApp::Application.config.secret_token = 'd9229c48e9c25961f9c140d2a1a9aefdb52be553490a28c17a45abae473ccf7b85e83266ae5489455dab56dbded34d8c71a5d316fc6a4d062a30c8c01937d79e'
